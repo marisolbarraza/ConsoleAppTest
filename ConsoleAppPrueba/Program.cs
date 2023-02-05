2e74e87eb41ec191d5eaae6cfd9d590054203d94
Marisol Barraza <marisolbarraza08@gmail.com>
2023-02-05 16:27:11 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleAppPrueba/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPrueba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region comentarios
+            /*Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sodales nulla urna. Donec pharetra pulvinar 
+             metus non vestibulum. Ut tincidunt felis non viverra porta. Praesent lorem risus, hendrerit in rutrum eget */
+            //Soy un simple comentario de línea
+            #endregion
+            Console.WriteLine("Holi mundo");
+
+        }
+    }
+}
